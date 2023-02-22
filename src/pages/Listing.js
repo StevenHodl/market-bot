47559b4f9bd998edab8a58394b643b0f79d77e5a
StevenHodl <StevenHodl@users.noreply.github.com>
2023-02-22 23:45:38 +0100

    dummy data for detail page

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import PageWrapper from "../components/commons/PageWrapper";
 import "./Listing.css";
-import { Avatar, Rate } from "antd";
+import { Avatar, Rate, Tag } from "antd";
 import SlideShow from "../components/commons/SlideShow";
 
 export function Listing() {
@@ -13,7 +13,6 @@ export function Listing() {
   let user_id;
 
   useEffect(() => {
-    console.log(id);
     if (id != undefined) {
       axios
         .get(localStorage.getItem("backend_url") + "/api/post/" + id)
@@ -41,13 +40,15 @@ export function Listing() {
             {/* <img src={listingDetails.thumbnail} alt={listingDetails.title} /> */}
           </div>
           <div className="main_info">
-            <h5>{listingDetails.category}</h5>
-            <h3>{listingDetails.title}</h3>
-            <p>id: {listingDetails.id}</p>
+            <Tag color="volcano">Market</Tag>
+            <h3>This is a post title</h3>
+            <p>post id: 1234567</p>
             <div className="sponsor_info">
-              <Avatar>NU</Avatar>
-              <p>{userDetails.name}</p>
-              <p>Level: OG</p>
+              <span>
+                <Avatar>NU</Avatar>
+                <p>Name Surname</p>
+              </span>
+              <p>Level: 2</p>
               <p>Posting since: 12/Feb/2022</p>
               <span>
                 Rating:
@@ -70,13 +71,15 @@ export function Listing() {
         </div>
         <div className="bottom_section">
           <div className="summary_info">
-            <p>Brand: {listingDetails.brand}</p>
+            <p>Posted on: 20/Feb/2023 20:43</p>
+            <p>Venezia (VE)</p>
+            <p>Brand: Samsung</p>
             <p>Status: Used</p>
             <p>Condition: Good Conditions</p>
           </div>
           <div className="detailed_info">
             <h6>Description</h6>
-            <p>{listingDetails.description}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
       </div>
